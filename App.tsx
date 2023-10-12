718c2badd0772be9f2c1cab762a1dde40c6aa322
Nekuin <nekuin@gmail.com>
2023-10-12 10:55:29 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {View} from 'react-native';
+
+import {TextInput} from 'react-native-paper';
+
+function App(): JSX.Element {
+  const [text, setText] = useState('');
+  return (
+    <View>
+      <TextInput
+        mode="outlined"
+        value={text}
+        onChangeText={txt => setText(txt)}
+        label="Something"
+      />
+    </View>
+  );
+}
+
+export default App;
